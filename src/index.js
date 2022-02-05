ec77bc49989cda9f4a4fc75ba4ed203366f8d750
anubhav <anubhavajmera25@gmail.com>
2022-02-05 17:16:30 +0530

    created a basic react app which gives me list of top 3 series on netflix and also redirect me to them

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Cards from './Cards'
+import './index.css'
+import Sdata from './Sdata'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <h1 className='heading_style'>List of top 3 series on Netflix in 2022</h1>
+
+    {Sdata.map((val) =>{
+      console.log(val)
+      return(
+        <Cards 
+        imgsrc={val.imgsrc}
+        title={val.title}
+        sname={val.sname}
+        link={val.link}
+        />
+      )
+    }
+
+    )}
+
+  </>,
+  document.getElementById('root')
+)
